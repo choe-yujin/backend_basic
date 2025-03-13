9449cda2a26e408b6bbf5ca163687ca368412748
choe-yujin <dev.yujinchoe@gmail.com>
2025-03-13 18:39:34 +0900

    refact: 요일 출력 기능 메소드 모듈화 리팩토링

--- Exercise2.java
+++ Exercise2.java
@@ -1,16 +1,37 @@
+import java.util.Scanner;
+
 public class Exercise2 {
     public static void main(String[] args) {
-        System.out.println(printWeek(new int[]{1, 3, 5, 8}));
+        int[] inputArr = getInputIntArray();
+        String[] dayArr = decideDay(inputArr);
+        System.out.println(printWeek(dayArr));
     }
 
     // 1. 입력 정수 배열을 매개변수로 받는 메소드를 정의한다. {1,3,5,8}
-    public static String printWeek(int[] input) {
+    public static int[] getInputIntArray() {
+        System.out.println("배열의 크기를 입력해주세요: ");
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt(); // 배열 크기 입력받기
 
-        String[] result = new String[input.length];
+        int[] inputArr = new int[size]; // 해당 크기의 배열 생성
+        System.out.println("배열의 요소들을 입력해주세요: ");
+        // 배열의 각 요소를 입력받아 저장
+        for (int i = 0; i < size; i++) {
+            inputArr[i] = sc.nextInt(); // 각 요소 입력받기
+        }
+
+        return inputArr; // 배열 반환
+    }
+
+
+    // 2. for문을 사용하여 배열의 각 요소를 순회한다.
+    // 3. switch 문을 사용하여 각 숫자에 해당하는 요일 문자열을 결정한다.
+    public static String[] decideDay(int[] inputArr) {
+        String[] result = new String[inputArr.length];
         int idx = 0;
 
         // 2. for문을 사용하여 배열의 각 요소를 순회한다.
-        for (int value : input) {
+        for (int value : inputArr) {
             // 3. switch 문을 사용하여 각 숫자에 해당하는 요일 문자열을 결정한다.
             switch (value) {
                 case 1:
@@ -40,7 +61,12 @@ public class Exercise2 {
             idx++;
         }
 
-        // 4. 결정된 요일 문자열을 콤마로 연결하여 결과 문자열을 생성 후 반환한다.
+        return result;
+    }
+
+    // 4. 결정된 요일 문자열을 콤마로 연결하여 결과 문자열을 생성 후 반환한다.
+    public static String printWeek(String[] result) {
         return String.join(", ", result);
     }
+
 }
